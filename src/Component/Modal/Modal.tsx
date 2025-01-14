bcdfec2947b7c30ec52763633f6acca38cfc9263
Тагир Ярмеев <yarmeevbi3@gmail.com>
2025-01-14 10:12:40 +0300

    add fetch from modal to api, telegram

--- src/Component/Modal/Modal.tsx
+++ src/Component/Modal/Modal.tsx
@@ -3,6 +3,7 @@ import "./Modal.css";
 import lefttop from "../../Assets/Modal/topleft.png";
 import righttop from "../../Assets/Modal/topright.png";
 import rightbottom from "../../Assets/Modal/bottomright.png";
+import skrepka from "../../Assets/Modal/skrepka.png";
 
 interface ModalProps {
   onClose: () => void;
@@ -17,6 +18,7 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
   const [phone, setPhone] = useState<string>("");
   const [message, setMessage] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const updatePlaceHolder = () => {
     if (window.innerWidth < 656) {
@@ -34,37 +36,93 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
     };
   }, []);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    setFile(selectedFile || null);
+    console.log("Выбранный файл:", selectedFile);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     // Проверка валидности данных
-    if (!name || !phone || !message) {
-      setError("Все поля обязательны для заполнения");
+    if (!name || !phone) {
+      setError("Имя и телефон обязательны для заполнения");
       return;
     }
 
     // Проверка формата телефона (пример)
     const phoneRegex = /^[0-9]{11}$/; // Пример для 10-значного номера
     if (!phoneRegex.test(phone)) {
-      setError("Некорректный номер телефона");
+      setError("Некорректный номер телефона"); // Переделать красиво через css
       return;
     }
 
-    // Если всё верно, отправить данные
-    const formData = {
-      name,
-      phone,
-      message,
-    };
+    setError(null);
 
-    console.log(formData); // Здесь вы можете отправить данные на сервер или сделать с ними что-то еще
+    const formData = new FormData();
+    formData.append("chat_id", process.env.REACT_APP_CHAT_ID || "");
+    formData.append("name", name);
+    formData.append("phone", phone);
+    formData.append("message", message);
 
-    // Успешная отправка данных
-    onClose(); // Закрываем модальное окно после успешной отправки
+    if (file) {
+      formData.append("document", file);
+      formData.append("caption", message);
+    } else {
+      console.log("Файла нет у клиента");
+    }
+
+    /* if (message && file) {
+      formData.append("message", message);
+      formData.append("document", file)
+    } else if (message && !file) {
+      formData.append("message", message)
+    } 
+    if (message) {
+      formData.append("message", message);
+    } else if (file) {
+      formData.append("document", file);
+    } else if (!file && !message) {
+      console.log("Нужно отправить или сообщение, или файл")
+    } */
+    /* if (file && message) {
+      formData.append("caption", message)
+    } */
+
+    /* formData.append(
+      "text",
+      `Имя: ${name}\nТелефон: ${phone}\nСообщение: ${message || ""}`
+    ); */
+
+    console.log("FormData перед отправкой:", {
+      chat_id: process.env.REACT_APP_CHAT_ID || "",
+      text: `Имя: ${name}\nТелефон: ${phone}\nСообщение: ${message || ""}`,
+      file: file ? file.name : null, // Показываем имя файла, если он есть
+    });
+
+    try {
+      const response = await fetch("http://localhost:5000/sendMessage", {
+        method: "POST",
+        body: formData,
+      });
+
+      const resData = await response.json();
+      if (!response.ok) {
+        throw new Error(resData.message || "Ошибка при отправке сообщения");
+      }
+
+      console.log("Response:", resData);
+    } catch (error) {
+      console.error("Ошибка при отправке сообщения: ", error);
+      setError("Произошла ошибка при отправке сообщения");
+    }
   };
 
   // Обработчик клика для фона модального окна
-  const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
+  const handleBackgroundClick = (
+    event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
+  ) => {
     // Закрываем модальное окно только, если клик был не на контейнере
     if (event.currentTarget === event.target) {
       onClose();
@@ -111,8 +169,31 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
                 placeholder={placeholder}
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
-                required
               ></textarea>
+              <input
+                type="file" // Поле для загрузки файла
+                name="document"
+                accept=".txt, .jpg, .png, .pdf" // Указываем типы файлов, которые можно загрузить
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+                id="file-upload"
+              />
+              <label
+                htmlFor="file-upload"
+                
+                className="fileUpload"
+              >
+                <span style={{ marginRight: "8px" }}>
+                  <img
+                    src={skrepka}
+                    alt="Upload"
+                    width="24"
+                    height="24"
+                  />
+                </span>
+                <span>{file ? file.name : "Прикрепить файл"}</span>{" "}
+                
+              </label>
               <button type="submit">
                 ОТПРАВИТЬ
                 <div className="arrow-up-modal">↑</div>
